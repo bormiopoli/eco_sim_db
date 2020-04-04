4d7bb931a9b960190323670f62eb2334e26fe106
birrachiara <bormiopoli@gmail.com>
2020-04-04 23:55:52 +0200

    upgrades

--- db_connection.py
+++ db_connection.py
@@ -24,63 +24,80 @@ def create_user_table(conn):
     :param project:
     :return: project id
     """
-    sql0 = """CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, 
-        balance real unsigned CHECK (balance >= 0), wealth REAL, bank integer)"""
+    sql = "DROP TABLE IF EXISTS users;"
     cur = conn.cursor()
+    cur.execute(sql)
+    sql = """CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, 
+        balance real unsigned CHECK (balance >= 0), wealth REAL, bank integer)"""
     try:
-        cur.execute(sql0)
+        cur.executescript(sql)
     except Exception as err:
         print(err)
     return cur.lastrowid
 
 
 def create_needs_table(conn):
-    sql = """CREATE TABLE IF NOT EXISTS needs (id integer PRIMARY KEY AUTOINCREMENT, user integer, product integer, 
-        product_name text, start_date date, end_date date, amount REAL, weight REAL, action text, satisfied BOOLEAN)"""
+    sql = "DROP TABLE IF EXISTS needs;"
     cur = conn.cursor()
     cur.execute(sql)
+    sql = """CREATE TABLE IF NOT EXISTS needs (id integer PRIMARY KEY AUTOINCREMENT, user integer, product integer, 
+        product_name text, start_date date, end_date date, amount REAL, weight REAL, action text, satisfied BOOLEAN)"""
+    cur.executescript(sql)
     return
 
 def create_activity_table(conn):
-    sql = """CREATE TABLE IF NOT EXISTS activity (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, amount REAL, energy REAL, impact REAL, user integer, bank integer, seller integer, product integer)"""
+    sql = "DROP TABLE IF EXISTS activity;"
     cur = conn.cursor()
     cur.execute(sql)
+    sql = """CREATE TABLE IF NOT EXISTS activity (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, amount REAL,
+        energy REAL, impact REAL, user integer, bank integer, seller integer, product integer, user_balance REAL, producer_balance REAL)"""
+    cur.executescript(sql)
     return
 
 def create_products_table(conn):
-    sql = """CREATE TABLE IF NOT EXISTS products (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, name text, amount REAL, energy REAL, impact REAL)"""
+    sql = "DROP TABLE IF EXISTS products;"
     cur = conn.cursor()
     cur.execute(sql)
+    sql = """CREATE TABLE IF NOT EXISTS products (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, product_name text, amount REAL, energy REAL, impact REAL, producer INTEGER)"""
+    cur.executescript(sql)
 
-# def update_products_table(conn, name, weight, amount, energy_per_material, impact_per_material):
-#
-#     sql = """UPDATE products SET end_date = CURRENT_TIMESTAMP, amount = REAL WHERE name like '{1}' AND id = {0} amount REAL, energy REAL, impact REAL)"""
-
-
-# def create_producer_table(conn):
-#     sql = """CREATE TABLE IF NOT EXISTS producers (user integer PRIMARY KEY, product integer, product_name text, start_date date, end_date date, energy REAL, impact REAL)"""
-#     cur = conn.cursor()
-#     cur.execute(sql)
-
+def create_produceable_product(conn):
+    sql = "DROP TABLE IF EXISTS produceable_products;"
+    cur = conn.cursor()
+    cur.execute(sql)
+    sql = """CREATE TABLE IF NOT EXISTS produceable_products (product_name TEXT, energy REAL, impact REAL);"""
+    res = cur.execute(sql)
+    sql = """INSERT INTO produceable_products (product_name, energy, impact) VALUES ('bread', 0.01, 0.001);"""
+    cur.execute(sql)
+    sql = """INSERT INTO produceable_products (product_name, energy, impact) VALUES ('eggs', 0.1, 0.01);"""
+    cur.execute(sql)
+    sql = """INSERT INTO produceable_products (product_name, energy, impact) VALUES ('cavial', 1, 0.1);"""
+    cur.execute(sql)
+    res = res.fetchall()
+    return res
 
 def create_bank_table(conn):
-    sql = """CREATE TABLE IF NOT EXISTS bank (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, amount REAL)"""
+    sql = "DROP TABLE IF EXISTS bank;"
     cur = conn.cursor()
     cur.execute(sql)
+    sql = """CREATE TABLE IF NOT EXISTS bank (id integer PRIMARY KEY AUTOINCREMENT, start_date date, end_date date, amount REAL)"""
+    cur.executescript(sql)
     return cur.lastrowid
 
 def create_loan_table(conn):
-    sql = "CREATE TABLE IF NOT EXISTS loans (id integer PRIMARY KEY AUTOINCREMENT, user integer, bank integer, amount REAL, energy REAL, impact REAL)"
+    sql = "DROP TABLE IF EXISTS loans;"
     cur = conn.cursor()
     cur.execute(sql)
+    sql = "CREATE TABLE IF NOT EXISTS loans (id integer PRIMARY KEY AUTOINCREMENT, user integer, bank integer, amount REAL, energy REAL, impact REAL)"
+    cur.executescript(sql)
     return cur.lastrowid
 
-def satisfy_need(conn, user, product, product_name, amount, weight, action):
-    sql = """UPDATE needs SET end_date = CURRENT_TIMESTAMP, satisfied = TRUE 
-        WHERE user = {0} AND product = {1} AND product_name like '{2}' AND amount = {3} 
-        AND weight = {4} AND action like '{5}' """.format(user, product, product_name, amount, weight, action)
+def satisfy_need(conn, user, product, amount, action):
+    sql = """UPDATE needs SET end_date = CURRENT_TIMESTAMP, satisfied = 1 
+        WHERE user = {0} AND product = {1} AND amount = {2} 
+        AND action like '{3}' """.format(user, product, amount, action)
     cur = conn.cursor()
-    cur.execute(sql)
+    cur.executescript(sql)
     return cur.lastrowid
 
 def change_need(conn, user, product, product_name, amount, weight, action):
@@ -88,23 +105,48 @@ def change_need(conn, user, product, product_name, amount, weight, action):
         AND product_name like '{2}' AND action like '{5}'
         """.format(user, product, product_name, amount, weight, action)
     cur = conn.cursor()
-    cur.execute(sql)
+    cur.executescript(sql)
     return cur.lastrowid
 
 def get_user(conn):
     cur = conn.cursor()
-    res = cur.execute("SELECT id FROM users")
+    res = cur.execute("SELECT id, start_date, end_date, balance, wealth, bank FROM users")
     res = res.fetchall()
     return res
 
-def get_productionneed(conn, user):
+def get_user_balance(conn, user):
     cur = conn.cursor()
-    res = cur.execute("SELECT product, product_name FROM needs WHERE user = {0} ;".format(user[0]))
+    res = cur.execute("SELECT balance FROM users WHERE id = {0}".format(user))
+    res = res.fetchall()
+    return res[0]
+
+def update_user_balance(conn, user, balance):
+    cur = conn.cursor()
+    res = cur.executescript("UPDATE users SET balance = {1} WHERE id = {0}".format(user, balance))
+    res = res.fetchall()
+    return res
+
+def update_loan(conn, user, bank, amount_to_add, new_energy, new_impact):
+    cur = conn.cursor()
+    res = cur.execute("SELECT amount, energy, impact FROM loans WHERE user = {0} AND bank = {1}".format(user, bank))
+    res = res.fetchall()
+    res = res[0] if res is not None and len(res)>0 else (0,0,0)
+    current_loan_amount = res[0]; current_consumption = res[1]; current_impact = res[2]
+    new_loan_amount = current_loan_amount + amount_to_add
+    new_energy = current_consumption + new_energy
+    new_impact = current_impact + new_impact
+    res = cur.executescript("UPDATE loans SET amount = {0}, energy = {1}, impact = {2} WHERE bank = {3} AND user = {4};".format(new_loan_amount, new_energy, new_impact, bank, user))
+    res = res.fetchall()
+    return res
+
+def add_user_bank(conn, chosen_bank, user):
+    cur = conn.cursor()
+    res = cur.executescript("UPDATE users SET bank = {0} WHERE id = {1};".format(chosen_bank, user))
     res = res.fetchall()
     return res
 
 def get_banks_byuser(conn, user):
-    sql = "SELECT bank FROM users WHERE id = {0}; ".format(user[0])
+    sql = "SELECT bank FROM users WHERE id = {0}; ".format(user)
     cur = conn.cursor()
     res = cur.execute(sql)
     res = res.fetchall()
@@ -118,49 +160,64 @@ def get_banks(conn):
     return res
 
 def get_product(conn):
-    sql = """SELECT id, name FROM products;"""
+    sql = """SELECT id, product_name, producer, energy, impact FROM products;"""
     cur = conn.cursor()
     res = cur.execute(sql)
     res = res.fetchall()
     return res
 
-def insert_need(conn, user, product, product_name, amount, weight, action):
-    sql = """INSERT INTO needs (user, product, product_name, start_date, amount, weight, action) VALUES ({0}, {1}, '{2}', CURRENT_TIMESTAMP, {3}, {4}, '{5}');""".format(
-        user[0], product, product_name, amount, weight, action)
+def get_produceable_product(conn):
+    sql = """SELECT product_name, energy, impact FROM produceable_products;"""
     cur = conn.cursor()
-    cur.execute(sql)
+    res = cur.execute(sql)
+    res = res.fetchall()
+    return res
+
+def get_unsatisfied_need_by_product(conn, product_id):
+    sql = """SELECT id, user, product, amount, weight FROM needs WHERE product = {0} AND action='BUY' AND satisfied = 0;""".format(product_id[0])
+    cur = conn.cursor()
+    res = cur.execute(sql)
+    res = res.fetchall()
+    return res
+
+def insert_need(conn, user, product, product_name, amount, weight, action, satisfied):
+    sql = """INSERT INTO needs (user, product, product_name, start_date, amount, weight, action, satisfied) VALUES ({0}, {1}, '{2}', CURRENT_TIMESTAMP, {3}, {4}, '{5}', {6});""".format(
+        user, product, product_name, amount, weight, action, satisfied)
+    cur = conn.cursor()
+    cur.executescript(sql)
     return cur.lastrowid
 
 def insert_user(conn, balance, wealth):
     sql = """INSERT INTO users(start_date, balance, wealth) VALUES(CURRENT_TIMESTAMP,{0},{1}); """.format(balance, wealth)
     cur = conn.cursor()
-    cur.execute(sql)
+    cur.executescript(sql)
     return cur.lastrowid
 
-def insert_activity(conn, amount, user, seller, bank, prod_name, energy_per_material, impact_per_material):
+def insert_activity(conn, amount, user, seller, prod_name, energy, impact, user_balance, producer_balance):
     cur = conn.cursor()
-    sql = """INSERT INTO activity (start_date, end_date, amount, energy, impact, user, bank, seller, product) VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, {1}, {2}, {3}, {4}, {0}, {5}, {6});""".format(
-        bank, amount, energy_per_material, impact_per_material, user, seller, prod_name)
-    cur.execute(sql)
+    sql = """INSERT INTO activity (start_date, end_date, amount, user, seller, product, energy, impact, user_balance, producer_balance) 
+        VALUES (CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7} );""".format(
+        amount, user, seller, prod_name, energy, impact, user_balance, producer_balance)
+    cur.executescript(sql)
     return cur.lastrowid
 
-def insert_products(conn, prod_name, energy_per_material, impact_per_material):
+def insert_products(conn, prod_name, energy_per_material, impact_per_material, producer):
     cur = conn.cursor()
-    sql = """INSERT INTO products (start_date, name, energy, impact) VALUES (CURRENT_TIMESTAMP, '{0}', {1}, {2} );""".format(
-        prod_name, energy_per_material, impact_per_material)
-    cur.execute(sql)
+    sql = """INSERT INTO products (start_date, product_name, energy, impact, producer) VALUES (CURRENT_TIMESTAMP, '{0}', {1}, {2}, {3} );""".format(
+        prod_name, energy_per_material, impact_per_material, producer)
+    cur.executescript(sql)
     return cur.lastrowid
 
 def insert_bank(conn):
     sql = """INSERT INTO bank (start_date) VALUES (CURRENT_TIMESTAMP);"""
     cur = conn.cursor()
-    cur.execute(sql)
+    cur.executescript(sql)
     return cur.lastrowid
 
 def insert_loan(conn, bank, user, amount, energy, impact ):
-    sql = """INSERT INTO loans (user, bank, amount, energy, impact) VALUES ({1}, {0}, {2}, {3}, {4}); """.format(bank[0], user[0], amount, energy, impact)
+    sql = """INSERT INTO loans (user, bank, amount, energy, impact) VALUES ({1}, {0}, {2}, {3}, {4}); """.format(bank, user, amount, energy, impact)
     cur = conn.cursor()
-    cur.execute(sql)
+    cur.executescript(sql)
     return cur.lastrowid
 
 
--- main3.py
+++ main3.py
@@ -1,61 +1,162 @@
 import numpy as np
 import random
-from classes import User, CO2_PRICE, ELECTRICITY_PRICE, Reservoir, Bank
+from classes3 import User, CO2_PRICE, ELECTRICITY_PRICE, Reservoir, Bank
 from collections import OrderedDict
 import matplotlib
 from time import time
 from classes3 import produce,sell, Activity, Bank, User, Product, Needs, Loans
 matplotlib.use('TkAgg')
+from db_connection import init_connection
 import matplotlib.pyplot as plt
 import tkinter
 import threading
-from microdm import find_seller, choice
+# from microdm import find_seller, choice
+from random import choice
+from db_connection import insert_activity,insert_bank, insert_loan, insert_need, insert_products, insert_user
+from db_connection import create_activity_table,create_bank_table,create_loan_table,create_needs_table,create_products_table,create_user_table, create_produceable_product
+from db_connection import get_banks, get_banks_byuser, get_product,get_user, get_produceable_product, get_unsatisfied_need_by_product, get_user_balance
+from db_connection import change_need, update_user_balance, update_loan, satisfy_need, add_user_bank
 
 
 NUMBER_OF_SALES = 2
 NUMBER_OF_USERS = 6
 NUMBER_OF_REPEATS = 10
 NAME_OF_BANKS = ["Vattenfall", "Shell"]
-NAME_OF_PRODUCTS = ["Bread", "Eggs", "Cavial"]
+NAME_OF_PRODUCTS = ["bread", "eggs", "cavial"]
 
 
-def generate_dict():
-    myDict = {"bread": {"weight": 8, "price":4, "producer": True, "amount": 0.5, "energy": 0.01, "impact": 0.001},
-              "eggs": {"weight": 1.7, "price":10, "producer": False, "amount": 0.1, "energy": 0.01, "impact": 0.001},
-              "cavial": {"weight": 0.1, "price": 100, "producer": True, "amount": 0.01, "energy": 0.01, "impact": 0.001}}
 
-    return myDict
+# def generate_dict():
+#     myDict = {"bread": {"weight": 8, "price":4, "producer": True, "amount": 0.5, "energy": 0.01, "impact": 0.001},
+#               "eggs": {"weight": 1.7, "price":10, "producer": False, "amount": 0.1, "energy": 0.01, "impact": 0.001},
+#               "cavial": {"weight": 0.1, "price": 100, "producer": True, "amount": 0.01, "energy": 0.01, "impact": 0.001}}
+#
+#     return myDict
+# myDict = generate_dict()
 
+
+def simulation(i):
+    # USER CHOOSE NEEDS OF PRODUCTS WITH RANDOM PARETIAN AMOUNT AND WEIGHT
+    for user in get_user(conn):
+        user_balance = user[3]
+        products_result = get_product(conn)
+        availability = user_balance / 2
+        while (availability > 0) and len(products_result) > 0:
+            chosen_product = choice(products_result)
+            if user[0] is not chosen_product[2]:
+                amount = random.paretovariate(3)
+                weight = amount
+                if (availability - (amount * weight)) >= 0:
+                    availability -= amount * weight
+                    insert_need(conn, user[0], chosen_product[0], chosen_product[1], amount, weight, "BUY", False)
+                    products_result.remove(chosen_product)
+                else:
+                    break
+            else:
+                products_result.remove(chosen_product)
+                continue
+
+
+    # FOR ALL NEEDS OF BUY NOT SATISFIED, CREATE AN ACTIVITY (aka TRANSACTION - Meant as production instantiation with randomized Energy and Impact values)
+    # for
+    products_result = get_product(conn)
+    for product_id in products_result:
+        needs_to_produce = get_unsatisfied_need_by_product(conn, product_id)
+        for need in needs_to_produce:
+            user = need[1]
+            amount = need[3]
+            weight = need[4]
+            energy = product_id[3] * amount
+            impact = product_id[4] * amount
+            producer = product_id[2]
+            product = product_id[0]
+            wealth = energy - impact
+            incentive = energy / impact if impact > 0 else -energy / abs(impact)
+
+            # plt.scatter(i, wealth, c='b', marker='x')
+            # plt.scatter(i, energy, c='y', marker='o')
+            # plt.scatter(i, impact, c='g', marker='x')
+
+            user_balance = get_user_balance(conn, user)
+            producer_balance = get_user_balance(conn, producer)[0]
+
+            if user_balance[0] >= wealth:
+                insert_activity(conn, amount, user, producer, product, energy, impact, user_balance[0], producer_balance)
+                update_user_balance(conn, user, (user_balance[0] - wealth))
+                satisfy_need(conn, user, product, amount, "BUY")
+
+            else:
+                print("USER DID NOT HAVE ENOUGH MONEY TO BUY")
+                continue
+
+            if energy > producer_balance:
+                banks = get_banks_byuser(conn, producer)
+                if banks is not None and len(banks) > 0:
+                    bank = choice(banks)
+                    update_loan(conn, producer, bank[0], (energy - producer_balance), energy, impact)
+                    update_user_balance(conn, producer, incentive)
+            else:
+                update_user_balance(conn, producer, incentive + producer_balance - energy)
 
 
 if __name__ == '__main__':
 
-    users = []
-    banks = []
-    products = []
-    needs = []
+    conn = init_connection()
+
+    create_user_table(conn)
+    create_products_table(conn)
+    create_needs_table(conn)
+    create_loan_table(conn)
+    create_bank_table(conn)
+    create_activity_table(conn)
+    create_produceable_product(conn)
 
     for i in range(NUMBER_OF_USERS):
-        users.append(User(100, None, None, None, None))
+        # users.append(User(100, None, None, None, None))
+        insert_user(conn, 100, 0)
 
     for i in range(len(NAME_OF_BANKS)):
-        banks.append(Bank(NAME_OF_BANKS[i], 10000))
+        # banks.append(Bank(NAME_OF_BANKS[i], 10000))
+        insert_bank(conn)
 
-    for i in range(len(NAME_OF_PRODUCTS)):
-        products.append(Product(NAME_OF_PRODUCTS[i]))
+    # HERE EACH USER CHOOSE A PRODUCT TO PRODUCE
+    produceable_products = get_produceable_product(conn)
+    for user in get_user(conn):
+        chosen_production = choice(produceable_products) if len(produceable_products)>0 else None
+        if chosen_production is not None and random.random() > 0.5:
+            produceable_products.remove(chosen_production)
+            insert_products(conn, chosen_production[0], chosen_production[1], chosen_production[2], user[0])
+    #
+    # for i in range(len(NAME_OF_PRODUCTS)):
+    #     # products.append(Product(NAME_OF_PRODUCTS[i]))
+    #     insert_products(conn, NAME_OF_PRODUCTS[i], myDict[NAME_OF_PRODUCTS[i]]['energy'], myDict[NAME_OF_PRODUCTS[i]]['impact'])
 
-    for user in users:
-        for product in products:
-            availability = user.balance / 2
-            while availability > 0:
-                if product.name is "Bread":
-                    needs.append(Needs(user, id(product), product.name, time(), None, 0.5, 8, "BUY" ))
-                if product.name is "Eggs":
-                    needs.append(Needs(user, id(product), product.name, time(), None, 0.1, 1.7, "BUY" ))
-                if product.name is "Cavial":
-                    needs.append(Needs(user, id(product), product.name, time(), None, 0.01, 0.1, "BUY" ))
+    # USER CHOOSE ONE RANDOM BANK
+    banks = get_banks(conn)
+    for user in get_user(conn):
+        chosen_bank = choice(banks)
+        user = user[0]; chosen_bank = chosen_bank[0]
+        insert_loan(conn, chosen_bank, user, 0, 0, 0)
+        add_user_bank(conn, chosen_bank, user)
 
-    for user in users:
-        user.choose_bank(banks)
-    for user in users:
-        user.choose_need(needs)
+    for i in range(NUMBER_OF_REPEATS):
+        simulation(i)
+
+    # a = "SELECT * FROM users join bank WHERE users.bank = bank.id AND users.id = 1"
+    print("FINISHED")
+
+    # for user in users:
+    #     for product in products:
+    #         availability = user.balance / 2
+    #         while availability > 0:
+    #             if product.name is "Bread":
+    #                 needs.append(Needs(user, id(product), product.name, time(), None, 0.5, 8, "BUY" ))
+    #             if product.name is "Eggs":
+    #                 needs.append(Needs(user, id(product), product.name, time(), None, 0.1, 1.7, "BUY" ))
+    #             if product.name is "Cavial":
+    #                 needs.append(Needs(user, id(product), product.name, time(), None, 0.01, 0.1, "BUY" ))
+    #
+    # for user in users:
+    #     user.choose_bank(banks)
+    # for user in users:
+    #     user.choose_need(needs)
